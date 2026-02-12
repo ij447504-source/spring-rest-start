4e9d3f92a264c635d110153f61033768ac1b6ca7
이지윤 <ij447504@gmail.com>
2026-02-12 18:05:27 +0900

    20260212 스프링세큐리티

--- src/main/java/com/metacoding/springv2/_core/filter/JwtAuthorizationFilter.java
+++ src/main/java/com/metacoding/springv2/_core/filter/JwtAuthorizationFilter.java
@@ -1,7 +1,33 @@
 package com.metacoding.springv2._core.filter;
 
+import java.io.IOException;
+
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 // 인가 필터
-public class JwtAuthorizationFilter {
-    
- 
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        System.out.println("~~~~~~~~~~~~~~~~~~~~JwtAuthorizationFilter");
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        // localhost:8080/good?username=ssar&password=1234
+        if (username.equals("ssar") && password.equals("1234")) {
+            filterChain.doFilter(request, response);
+        } else {
+            response.getWriter().println("get out");
+        }
+
+    }
+
 }

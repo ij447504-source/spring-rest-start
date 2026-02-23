1eb455c63f8bf12043c6e279a6e717dc919c26e3
이지윤 <ij447504@gmail.com>
2026-02-23 10:06:05 +0900

    20260223

--- src/main/java/com/metacoding/springv2/_core/config/SecurityConfig.java
+++ src/main/java/com/metacoding/springv2/_core/config/SecurityConfig.java
@@ -14,45 +14,47 @@ import com.metacoding.springv2._core.util.RespFilter;
 @Configuration
 public class SecurityConfig {
 
-    @Bean
-    public BCryptPasswordEncoder encode() {
-        return new BCryptPasswordEncoder();
-    }
+        @Bean
+        public BCryptPasswordEncoder encode() {
+                return new BCryptPasswordEncoder();
+        }
 
-    // 시큐리티 필터 등록
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.headers(headers -> headers
-                .frameOptions(frameOptions -> frameOptions.sameOrigin()));
+        // 시큐리티 필터 등록
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+                http.headers(headers -> headers
+                                .frameOptions(frameOptions -> frameOptions.sameOrigin()));
 
-        http.cors(c -> c.disable()); // 싴큐리티가 들고있는 CORS 비활성화
+                // http.cors(c -> c.disable()); // 싴큐리티가 들고있는 CORS 비활성화
 
-        http.exceptionHandling(ex -> ex
-                .authenticationEntryPoint(
-                        (request, response, authException) -> RespFilter.fail(response, 401, "로그인 후 이용해주세요"))
-                .accessDeniedHandler(
-                        (request, response, accessDeniedException) -> RespFilter.fail(response, 403, "권한이 없습니다")));
+                http.exceptionHandling(ex -> ex
+                                .authenticationEntryPoint(
+                                                (request, response, authException) -> RespFilter.fail(response, 401,
+                                                                "로그인 후 이용해주세요"))
+                                .accessDeniedHandler(
+                                                (request, response, accessDeniedException) -> RespFilter.fail(response,
+                                                                403, "권한이 없습니다")));
 
-        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
-        // 인증/권한 주소 커스터마이징
-        http.authorizeHttpRequests(authorize -> authorize
-                .requestMatchers("/api/**").authenticated()
-                .requestMatchers("/admin/**").hasRole("ADMIN")
-                .anyRequest().permitAll());
+                // 인증/권한 주소 커스터마이징
+                http.authorizeHttpRequests(authorize -> authorize
+                                .requestMatchers("/api/**").authenticated()
+                                .requestMatchers("/admin/**").hasRole("ADMIN")
+                                .anyRequest().permitAll());
 
-        // 폼 로그인 비활성화 ( POST : x-www-form-urlencoded : username, password )
-        http.formLogin(f -> f.disable());
+                // 폼 로그인 비활성화 ( POST : x-www-form-urlencoded : username, password )
+                http.formLogin(f -> f.disable());
 
-        // 베이직 인증 활성화 시킴 (request 할때마다 username, password를 요구)
-        http.httpBasic(b -> b.disable());
+                // 베이직 인증 활성화 시킴 (request 할때마다 username, password를 요구)
+                http.httpBasic(b -> b.disable());
 
-        // csrf 비활성화
-        http.csrf(c -> c.disable());
+                // csrf 비활성화
+                http.csrf(c -> c.disable());
 
-        // 인증 필터를 변경
-        http.addFilterBefore(new JwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+                // 인증 필터를 변경
+                http.addFilterBefore(new JwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
 
-        return http.build();
-    }
+                return http.build();
+        }
 }
